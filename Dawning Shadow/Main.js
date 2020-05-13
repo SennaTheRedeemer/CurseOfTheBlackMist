4a0231567d355540d89baf8e1b996833f7e45f91
SennaTheRedeemer <ofir.norani2@gmail.com>
2020-05-13 14:34:24 +0300

    Creating the animal table

--- Dawning Shadow/Main.js
+++ Dawning Shadow/Main.js
@@ -38,3 +38,45 @@ const padZeros = (time) => {
     if (time < 10) {time = "0" + time;}  // add zero in front of numbers < 10
     return time;
 }
+
+// Create the table headers
+$('#animalsTable').append('<tr id="header">');
+shortenedAnimalKeys.forEach((key) => {
+    $('#header').append(`<th> ${animalKeysHebrew[key]} </th>`);
+})
+
+// Create the table body
+animals.forEach((animal) => {
+    $('#animalsTable').append(`<tr id="${animal['id']}Row">`);
+    shortenedAnimalKeys.forEach((key) => {
+        $(`#${animal['id']}Row`).append(`<td name="${animal["id"]}"> ${animal[key]} </td>`);
+        $(`td[name =${animal["id"]}]`).click(() => {
+            // Set the name of the chosen animal 
+            $('#chosenAnimalName').text(`${animal["name"]}`);
+            // Add description of the chosen animal
+            $('#chosenAnimalDescription').text(() => {
+                let description = '';
+                displayAnimalKeys.forEach((key) => {
+                    description += `${animalKeysHebrew[key]}: ${animal[key]}, `;
+                })
+                return description.substring(0, description.length - 2) + '.';
+            });
+        });
+    });
+});
+
+
+$("td").not('').hover(
+    function() {
+        let parent = $(this).closest('tr')
+        parent.children().each(function() {
+            $(this).addClass( "hovered" )
+        })
+    }, 
+    function() {
+        let parent = $(this).parent()
+        parent.children().each(function() {
+            $(this).removeClass( "hovered" )
+        })
+    }
+  );
---
+++ Dawning Shadow/Zoo.js
@@ -0,0 +1,30 @@
+let animals = [
+    {
+        id: 100,
+        name: "ג'ירפה",
+        cage: 'ספארי',
+        legs: 4
+    },
+    {
+        id: 200,
+        name: 'פיל',
+        cage: 'ספארי',
+        legs: 4
+    },
+    {
+        id: 300,
+        name: 'דולפין',
+        cage: 'ים',
+        legs: 0
+    }
+];
+let displayAnimalKeys = ['name', 'cage', 'legs'];
+let animalKeysHebrew = 
+    {
+        id: 'מספר החיה',
+        name: 'שם החיה',
+        cage: 'כלוב',
+        legs: 'מספר רגליים'
+    };
+let cages = ['ים', 'ספארי', 'פינת ליטוף'];
+let shortenedAnimalKeys = ['id', 'name'];
