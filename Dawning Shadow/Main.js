273310077e1f0213adf03c3a44d3e4fcbff3d384
SennaTheRedeemer <ofir.norani2@gmail.com>
2020-05-13 10:52:15 +0300

    fixed standarts and style

--- Dawning Shadow/Main.js
+++ Dawning Shadow/Main.js
@@ -1,39 +1,40 @@
-var intervalTimeFunctionID = null
-var isClockOn = false
+let intervalID = null;
+let isClockOn = false;
 
 window.onload = () => {
-    changeTime()
+    changeTime();
 }
 
 const changeTime = () => {
-    var today = new Date()
+    const today = new Date();
 
-    var hours = today.getHours()
-    var minutes = today.getMinutes()
-    var seconds = today.getSeconds()
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
 
-    minutes = checkTime(minutes)
-    seconds = checkTime(seconds)
+    minutes = padZeros(minutes);
+    seconds = padZeros(seconds);
 
-    document.getElementById('clock').innerHTML = `<i class="far fa-clock" style="padding-left:5px"></i>` + hours + ":" + minutes + ":" + seconds 
+    document.getElementById('clock').innerHTML = `<i class="far fa-clock" style="padding-left:5px"></i> ${hours}:${minutes}:${seconds}`;
 }
 
 const clockButtonClick = () => {
     if (isClockOn) {
-        clearInterval(intervalTimeFunctionID)
-        isClockOn = false
-        document.getElementById('clockButton').innerHTML = "Turn ON"
+        clearInterval(intervalID);
+        isClockOn = false;
+        document.getElementById('clockButton').innerHTML = "Turn ON";
         document.getElementById('clockButton').style.backgroundColor = "lightgreen";
     }
     else {
-        intervalTimeFunctionID = setInterval(changeTime, 500)
-        isClockOn = true
-        document.getElementById('clockButton').innerHTML = "Turn OFF"
+        changeTime()
+        intervalID = setInterval(changeTime, 1000);
+        isClockOn = true;
+        document.getElementById('clockButton').innerHTML = "Turn OFF";
         document.getElementById('clockButton').style.backgroundColor = "lightcoral";
     }
 }
 
-const checkTime = (time) => {
-    if (time < 10) {time = "0" + time}  // add zero in front of numbers < 10
-    return time
+const padZeros = (time) => {
+    if (time < 10) {time = "0" + time;}  // add zero in front of numbers < 10
+    return time;
 }
